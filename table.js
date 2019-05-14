078044ac1554c39d60d4a0bc182659f12be2ceac
adaephonben <ma18btech11011@iith.ac.in>
2019-05-14 17:10:17 +0530

    Added preliminary scraping code

--- table.js
+++ table.js
@@ -1,5 +1,15 @@
+function eventFire(el, etype){
+  if (el.fireEvent) {
+    el.fireEvent('on' + etype);
+  } else {
+    var evObj = document.createEvent('Events');
+    evObj.initEvent(etype, true, false);
+    el.dispatchEvent(evObj);
+  }
+}
+
 var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-var slots = ["09:00-09:55", "10:00-10:55", "11:00-11:55", "12:00-12:55", "02:30-03:55", "04:00-05:25"] // Add 5:30-7 later
+var slots = ["09:00-09:55", "10:00-10:55", "11:00-11:55", "12:00-12:55", "14:30-15:55", "16:00-17:25"] // Add 5:30-7 later
 
 var parser = new DOMParser();
 var DOM = parser.parseFromString(localStorage.getItem("DOM"), "text/html");
@@ -14,14 +24,15 @@ while (true)
 	var courseCodeID = "cCd_"+currentID+"_"+data_deg_dtl+"_1";
 	var timetableRowsClass = "timeTabTr_"+currentID+"_"+data_deg_dtl+"_1";
 	var currentTimetableRows = DOM.getElementsByClassName(timetableID);
+	var spanID = "timeTab_"+currentID+"_"+data_deg_dtl+"_1";
+	eventFire(document.getElementById(spanID), "click");
 	var currentCourseCodeInput = DOM.getElementById(courseCodeID);
 	if (currentTimetableDiv == null)
 		break ;
 
 	for (var i = 0 ; i < currentTimetableRows.length ; i++)
 	{
-		
+		console.log(currentTimetableRows[i]);
 	}
-
 	currentID += 1 ;
 }
