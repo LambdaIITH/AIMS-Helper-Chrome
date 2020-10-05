6e73155e49f031e0031681d779fc2b6b18063e90
Tarun Noone <56215355+TarunNoone@users.noreply.github.com>
2020-10-05 19:11:58 +0530

    Added Husky, ESLint Airbnb Config and Prettier

--- src/gpa/activateGPA.js
+++ src/gpa/activateGPA.js
@@ -2,102 +2,102 @@
 Source: https://github.com/IITH/aims-gpa-calculator
 */
 
-var exclude_list = [
+const exclude_list = [
   'Minor core',
   'Honors core',
   'Honours project',
   'Honours coursework',
   'FCC',
-  'Additional'
+  'Additional',
 ];
-var grade_values = {
+const grade_values = {
   'A+': 10,
-  'A': 10,
+  A: 10,
   'A-': 9,
-  'B': 8,
+  B: 8,
   'B-': 7,
-  'C': 6,
+  C: 6,
   'C-': 5,
-  'D': 4,
-  'FR': 0,
-  'FS': 0
+  D: 4,
+  FR: 0,
+  FS: 0,
 };
-//console.log(studentId);
-append_checkbox = function(parent, is_checked){
-    parent.append("<input class=\"cgpa_cal_check\" type=\"checkbox\" "+(is_checked?"checked":"")+" />");
-}
-add_checkboxes = function(){
-    var courses_checked = new Set();
-    $(".cgpa_cal_check").remove();
-    elems = $(".hierarchyLi.dataLi").not(".hierarchyHdr, .hierarchySubHdr");
-    elems.each(function(i){
-        var course_id = $(this).children(".col1").html().trim();
-        if (courses_checked.has(course_id)){
-            append_checkbox($(this).children(".col1"), false);
-            return;
-        }
-        is_checked = true;
-        type = $(this).children(".col5").html().trim().slice(6);
-        grade = $(this).children(".col8").html().trim().slice(6);
-        console.log(grade, grade.length);
-        if (exclude_list.indexOf(type) > -1 || grade == "" || grade == "I")
-            is_checked = false;
-        if (is_checked)
-            courses_checked.add(course_id);
-        append_checkbox($(this).children(".col1"), is_checked);
-    });
-}
+// console.log(studentId);
+append_checkbox = function (parent, is_checked) {
+  parent.append(`<input class="cgpa_cal_check" type="checkbox" ${is_checked ? 'checked' : ''} />`);
+};
+add_checkboxes = function () {
+  const courses_checked = new Set();
+  $('.cgpa_cal_check').remove();
+  elems = $('.hierarchyLi.dataLi').not('.hierarchyHdr, .hierarchySubHdr');
+  elems.each(function (i) {
+    const course_id = $(this).children('.col1').html().trim();
+    if (courses_checked.has(course_id)) {
+      append_checkbox($(this).children('.col1'), false);
+      return;
+    }
+    is_checked = true;
+    type = $(this).children('.col5').html().trim()
+      .slice(6);
+    grade = $(this).children('.col8').html().trim()
+      .slice(6);
+    console.log(grade, grade.length);
+    if (exclude_list.indexOf(type) > -1 || grade == '' || grade == 'I') { is_checked = false; }
+    if (is_checked) { courses_checked.add(course_id); }
+    append_checkbox($(this).children('.col1'), is_checked);
+  });
+};
 
-show_total_gpa = function(){
-    var courses = [];
-    $('#gpa_button').val('Calculating');
-    $('#gpa_bar').remove();
-    total_grades = 0;
-    total_credits = 0;
-    if ($(".cgpa_cal_check").length==0)
-        add_checkboxes();
-    elems = $(".hierarchyLi.dataLi").not(".hierarchyHdr, .hierarchySubHdr");
-    var type_credits_map = new Map();
-    elems.each(function(i){
-        if ($(this).find(".cgpa_cal_check:checked").length == 0 )
-            return;
+show_total_gpa = function () {
+  const courses = [];
+  $('#gpa_button').val('Calculating');
+  $('#gpa_bar').remove();
+  total_grades = 0;
+  total_credits = 0;
+  if ($('.cgpa_cal_check').length == 0) { add_checkboxes(); }
+  elems = $('.hierarchyLi.dataLi').not('.hierarchyHdr, .hierarchySubHdr');
+  const type_credits_map = new Map();
+  elems.each(function (i) {
+    if ($(this).find('.cgpa_cal_check:checked').length == 0) { return; }
 
-        var course = new Object();
-        course.code = $(this).children(".col1").contents().filter(function(){
-            return this.nodeType == Node.TEXT_NODE;
-          }).text().trim();
-        course.name = $(this).children(".col2").html().trim();
-        course.type = $(this).children(".col5").html().trim().slice(6);
-        course.grade = $(this).children(".col8").html().trim().slice(6);
-        course.credits = Number($(this).children(".col3").html().trim().slice(6));
-        if (!(course.grade in grade_values))
-            return;
+    const course = new Object();
+    course.code = $(this).children('.col1').contents().filter(function () {
+      return this.nodeType == Node.TEXT_NODE;
+    })
+      .text()
+      .trim();
+    course.name = $(this).children('.col2').html().trim();
+    course.type = $(this).children('.col5').html().trim()
+      .slice(6);
+    course.grade = $(this).children('.col8').html().trim()
+      .slice(6);
+    course.credits = Number($(this).children('.col3').html().trim()
+      .slice(6));
+    if (!(course.grade in grade_values)) { return; }
 
-        if (type_credits_map.has(course.type))
-          type_credits_map.set(course.type, type_credits_map.get(course.type) + course.credits);
-        else
-          type_credits_map.set(course.type, course.credits);
+    if (type_credits_map.has(course.type)) { type_credits_map.set(course.type, type_credits_map.get(course.type) + course.credits); } else { type_credits_map.set(course.type, course.credits); }
 
-
-        grade_value = grade_values[course.grade];
-        credits = course.credits;
-        total_grades += credits * grade_value;
-        total_credits += credits;
-        courses.push(course);
-    });
-    console.log(total_grades, total_credits);
-    var gpa = (total_grades / total_credits).toFixed(2);
-    return {
-        name: $(".stuName").text().trim(),
-        rollno: $(".studentInfoDiv>.flexDiv:nth-child(3)").find("span").text().trim(),
-        branch: $(".studentInfoDiv>.flexDiv:nth-child(5)").find("div:nth-child(1)").find("span").text().trim(),
-        student_type: $(".studentInfoDiv>.flexDiv:nth-child(5)").find("div:nth-child(2)").find("span").text().trim(),
-        type_credits_map: JSON.stringify(Array.from(type_credits_map)),
-        courses: JSON.stringify(courses),
-        gpa: gpa
-    };
-}
+    grade_value = grade_values[course.grade];
+    credits = course.credits;
+    total_grades += credits * grade_value;
+    total_credits += credits;
+    courses.push(course);
+  });
+  console.log(total_grades, total_credits);
+  const gpa = (total_grades / total_credits).toFixed(2);
+  return {
+    name: $('.stuName').text().trim(),
+    rollno: $('.studentInfoDiv>.flexDiv:nth-child(3)').find('span').text().trim(),
+    branch: $('.studentInfoDiv>.flexDiv:nth-child(5)').find('div:nth-child(1)').find('span').text()
+      .trim(),
+    student_type: $('.studentInfoDiv>.flexDiv:nth-child(5)').find('div:nth-child(2)').find('span').text()
+      .trim(),
+    type_credits_map: JSON.stringify(Array.from(type_credits_map)),
+    courses: JSON.stringify(courses),
+    gpa,
+  };
+};
 chrome.runtime.sendMessage({
-    action: "parsedGPA",
-    data: show_total_gpa()
+  action: 'parsedGPA',
+  data: show_total_gpa(),
 });
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,34 +1,29 @@
-
-var whichButton = 0 ; // 0 : no button has been clicked
+let whichButton = 0; // 0 : no button has been clicked
 chrome.runtime.onMessage.addListener((request, sender) => {
-	if (request.action == "activateTimetable" && request.status == true)
-	{
-		onWindowLoad();
-	}
+  if (request.action == 'activateTimetable' && request.status == true) {
+    onWindowLoad();
+  }
 });
-chrome.runtime.onMessage.addListener(function(request, sender) {
-  if (request.action == "getSource") {
-    if (whichButton == 1)
-    {
-    	localStorage.setItem("DOM", request.source);
+chrome.runtime.onMessage.addListener((request, sender) => {
+  if (request.action == 'getSource') {
+    if (whichButton == 1) {
+    	localStorage.setItem('DOM', request.source);
     	chrome.runtime.sendMessage({
-    		action: "LaunchTimetableFile",
-    		source: "hello"
-    	}, function(data) {
+    		action: 'LaunchTimetableFile',
+    		source: 'hello',
+    	}, (data) => {
     		console.log(chrome.runtime.lastError.message);
-    	} );
-    }
-    else
-    {
-    	console.log("You shouldn't be here. "); // Um, just like that. 
+    	});
+    } else {
+    	console.log("You shouldn't be here. "); // Um, just like that.
     }
   }
 });
 
 function onWindowLoad() {
-	chrome.tabs.executeScript(null, {
-    file: "/src/timetable/getSource.js"
-  }, function() {
+  chrome.tabs.executeScript(null, {
+    file: '/src/timetable/getSource.js',
+  }, () => {
     if (chrome.runtime.lastError) {
       console.log(chrome.runtime.lastError.message);
     }
@@ -36,53 +31,53 @@ function onWindowLoad() {
 }
 
 function showLoading() {
-    document.getElementById("loading-image").style.display = "block";
-    document.getElementsByClassName("button-container")[0].style.display="none";
+  document.getElementById('loading-image').style.display = 'block';
+  document.getElementsByClassName('button-container')[0].style.display = 'none';
 }
 
 function removeLoading() {
-    document.getElementById("loading-image").style.display = "none";
-    document.getElementsByClassName("button-container")[0].style.display = "block";
+  document.getElementById('loading-image').style.display = 'none';
+  document.getElementsByClassName('button-container')[0].style.display = 'block';
 }
 
 function injectTimetable() {
-    showLoading();
-	chrome.tabs.executeScript(null, {
-		file: "/src/timetable/activateTimetable.js"
-	}, function() {
-		if (chrome.runtime.lastError)	console.log(chrome.runtime.lastError.message);
-	});
+  showLoading();
+  chrome.tabs.executeScript(null, {
+    file: '/src/timetable/activateTimetable.js',
+  }, () => {
+    if (chrome.runtime.lastError)	console.log(chrome.runtime.lastError.message);
+  });
 }
 
 function injectGPA() {
-    showLoading();
+  showLoading();
+  chrome.tabs.executeScript(null, {
+    file: '/js/jquery-3.4.1.min.js',
+  }, (result) => {
     chrome.tabs.executeScript(null, {
-        file: "/js/jquery-3.4.1.min.js"
-    }, function(result){
-        chrome.tabs.executeScript(null, {
-            file: "/src/gpa/activateGPA.js"
-        }, function() {
-            if(chrome.runtime.lastError) console.log(chrome.runtime.lastError.message);
-        });
+      file: '/src/gpa/activateGPA.js',
+    }, () => {
+      if (chrome.runtime.lastError) console.log(chrome.runtime.lastError.message);
     });
+  });
 }
-chrome.runtime.onMessage.addListener(function(request, sender){
-    if(request.action == "parsedGPA"){
-        removeLoading();
-        var gpa_value = request.data.gpa;
-        document.getElementsByClassName("gpa-container")[0].style.display = "flex";
-        document.getElementsByClassName("gpa-value")[0].innerText = gpa_value;
-        //TODO: use the courses and GPA to display in another tab.
+chrome.runtime.onMessage.addListener((request, sender) => {
+  if (request.action == 'parsedGPA') {
+    removeLoading();
+    const gpa_value = request.data.gpa;
+    document.getElementsByClassName('gpa-container')[0].style.display = 'flex';
+    document.getElementsByClassName('gpa-value')[0].innerText = gpa_value;
+    // TODO: use the courses and GPA to display in another tab.
 
-        localStorage.setItem("courseGPA", JSON.stringify(request.data));
-        chrome.tabs.create({ url: chrome.runtime.getURL("/src/gpa/gpa_report.html")});
-    }
+    localStorage.setItem('courseGPA', JSON.stringify(request.data));
+    chrome.tabs.create({ url: chrome.runtime.getURL('/src/gpa/gpa_report.html') });
+  }
 });
-document.getElementsByClassName("generate-timetable-button")[0].onclick = function() {
-	whichButton = 1 ; // timetable button click event
-	injectTimetable();
+document.getElementsByClassName('generate-timetable-button')[0].onclick = function () {
+  whichButton = 1; // timetable button click event
+  injectTimetable();
 };
-document.getElementsByClassName("calculate-gpa-button")[0].onclick = function() {
-	whichButton = 2 ; // GPA Button click event
-	injectGPA();
+document.getElementsByClassName('calculate-gpa-button')[0].onclick = function () {
+  whichButton = 2; // GPA Button click event
+  injectGPA();
 };
